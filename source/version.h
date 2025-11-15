ef3a7007a3b47dbffdf48d33b77f24136a8564e1
f3bandit <f3bandit@gmail.com>
2025-11-14 23:05:58 -0500

    Update kernel version to 1.06

--- source/version.h
+++ source/version.h
@@ -2,6 +2,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define KERNEL_VERSION "1.05"  // Define your kernel version
+#define KERNEL_VERSION "1.06"  // Define your kernel version
+
 
 #endif // VERSION_H
